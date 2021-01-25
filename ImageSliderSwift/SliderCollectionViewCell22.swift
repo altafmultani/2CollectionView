ef03dd3c6bae36cf6bd4fa92421d497e68c55340
altafmultani16 <altafmultani16@gmail.com>
2021-01-25 09:51:28 -0800

    initial

---
+++ ImageSliderSwift/SliderCollectionViewCell22.swift
@@ -0,0 +1,17 @@
+//
+//  SliderCollectionViewCell22.swift
+//  ImageSliderSwift
+//
+//  Created by helpmac on 21/01/21.
+//
+
+import UIKit
+
+class SliderCollectionViewCell22: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
